7b6bae0cfc1929b9774145a56d6818e61e15a0b8
dekimage <dejan.gavrilovikk@gmail.com>
2022-11-27 13:14:38 +0100

    add books

--- pages/book/[id].js
+++ pages/book/[id].js
@@ -1,107 +1,107 @@
-// import { useQuery } from "@apollo/react-hooks";
-// import { Context } from "../../context/store";
-// import { useRouter } from "next/router";
+import { useQuery } from "@apollo/react-hooks";
+import { Context } from "../../context/store";
+import { useRouter } from "next/router";
 
-// import Link from "next/link";
-// import _ from "lodash";
-// // import styles from "../../styles/Problem.module.scss";
-// import ReactMarkdown from "react-markdown";
-// import { Action } from "../../components/cardPageComps";
+import Link from "next/link";
+import _ from "lodash";
+// import styles from "../../styles/Problem.module.scss";
+import ReactMarkdown from "react-markdown";
+import { Action } from "../../components/cardPageComps";
 
-// import NavBar from "../../components/NavBar";
-// import ExpandableComponent from "../../components/ExpandableComponent";
-// import { normalize } from "../../utils/calculations";
-// import { Course } from "../../components/shopComps";
+import NavBar from "../../components/NavBar";
+import ExpandableComponent from "../../components/ExpandableComponent";
+import { normalize } from "../../utils/calculations";
+import { Course } from "../../components/shopComps";
 
-// import { GET_BOOK_ID } from "../../GQL/query";
+import { GET_BOOK_ID } from "../../GQL/query";
 
-// import iconSource from "../../assets/source.svg";
+import iconSource from "../../assets/source.svg";
 
-// import styles from "../../styles/Book.module.scss";
+import styles from "../../styles/Book.module.scss";
 
-// const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
-// const BookView = ({ book }) => {
-//   const {
-//     id,
-//     name,
-//     author,
-//     image,
-//     ideas,
-//     cards,
-//     actions,
-//     course,
-//     problems,
-//     realm,
-//   } = book;
+const BookView = ({ book }) => {
+  const {
+    id,
+    name,
+    author,
+    image,
+    ideas,
+    cards,
+    actions,
+    course,
+    problems,
+    realm,
+  } = book;
 
-//   const router = useRouter();
+  const router = useRouter();
 
-//   console.log(ideas);
+  console.log(ideas);
 
-//   return (
-//     <div>
-//       <div onClick={() => router.back()}>
-//         <ion-icon name="chevron-back-outline"></ion-icon>
-//       </div>
-//       <div className={styles.book_image}>
-//         {image && <img src={`${baseUrl}${image.url}`} height="160px" />}
-//       </div>
+  return (
+    <div>
+      <div onClick={() => router.back()}>
+        <ion-icon name="chevron-back-outline"></ion-icon>
+      </div>
+      <div className={styles.book_image}>
+        {image && <img src={`${baseUrl}${image.url}`} height="160px" />}
+      </div>
 
-//       <div className={styles.book_name}>{name}</div>
-//       <div className={styles.book_author}>By {author}</div>
+      <div className={styles.book_name}>{name}</div>
+      <div className={styles.book_author}>By {author}</div>
 
-//       <ExpandableComponent
-//         name={"5 Key Ideas"}
-//         children={
-//           <div>
-//             <ReactMarkdown children={ideas} />
-//           </div>
-//         }
-//       />
-//       <ExpandableComponent
-//         name={"5 Actions"}
-//         tag={"free"}
-//         children={
-//           <div>
-//             {actions.map((a, i) => (
-//               <Action action={a} key={i} parent="problem" />
-//             ))}
-//           </div>
-//         }
-//       />
+      <ExpandableComponent
+        name={"5 Key Ideas"}
+        children={
+          <div>
+            <ReactMarkdown children={ideas} />
+          </div>
+        }
+      />
+      <ExpandableComponent
+        name={"5 Actions"}
+        tag={"free"}
+        children={
+          <div>
+            {actions.map((a, i) => (
+              <Action action={a} key={i} parent="problem" />
+            ))}
+          </div>
+        }
+      />
 
-//       <ExpandableComponent
-//         name={"Program inspired by this book"}
-//         children={<Course course={course} />}
-//       />
-//     </div>
-//   );
-// };
+      <ExpandableComponent
+        name={"Program inspired by this book"}
+        children={<Course course={course} />}
+      />
+    </div>
+  );
+};
 
-// const BookPage = () => {
-//   const router = useRouter();
+const BookPage = () => {
+  const router = useRouter();
 
-//   const { data, loading, error } = useQuery(GET_BOOK_ID, {
-//     variables: { id: router.query.id },
-//   });
+  const { data, loading, error } = useQuery(GET_BOOK_ID, {
+    variables: { id: router.query.id },
+  });
 
-//   const gql_data = data && normalize(data);
+  const gql_data = data && normalize(data);
 
-//   return (
-//     <div className="background_dark">
-//       <div className="section">
-//         {error && <div>Error: {error}</div>}
-//         {loading && <div>Loading...</div>}
-//         {gql_data && (
-//           <div>
-//             <BookView book={gql_data.book} />
-//           </div>
-//         )}
-//       </div>
-//       <NavBar />
-//     </div>
-//   );
-// };
+  return (
+    <div className="background_dark">
+      <div className="section">
+        {error && <div>Error: {error}</div>}
+        {loading && <div>Loading...</div>}
+        {gql_data && (
+          <div>
+            <BookView book={gql_data.book} />
+          </div>
+        )}
+      </div>
+      <NavBar />
+    </div>
+  );
+};
 
-// export default BookPage;
+export default BookPage;
--- pages/books.js
+++ pages/books.js
@@ -1,130 +1,130 @@
-// import { useQuery } from "@apollo/react-hooks";
-// import Header from "../components/Header";
-// import NavBar from "../components/NavBar";
-// import Link from "next/link";
-// import { useEffect, useState } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
-// import { normalize } from "../utils/calculations";
-// import styles from "../styles/Books.module.scss";
+import { normalize } from "../utils/calculations";
+import styles from "../styles/Books.module.scss";
 
-// import { DropDown } from "../components/Dropdown";
+import { DropDown } from "../components/Dropdown";
 
-// import { GET_BOOKS, GET_REALMS } from "../GQL/query";
+import { GET_BOOKS, GET_REALMS } from "../GQL/query";
 
-// const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
-// export const GenericDropDown = ({ items, label, callback }) => {
-//   const [isOpen, setIsOpen] = useState(false);
-//   const [selectedItem, setSelectedItem] = useState(false);
+export const GenericDropDown = ({ items, label, callback }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(false);
 
-//   return (
-//     <div
-//       tabIndex={0}
-//       onBlur={(e) => {
-//         setIsOpen(false);
-//       }}
-//     >
-//       <div className={styles.dropDown} onClick={() => setIsOpen(!isOpen)}>
-//         <div className="mr5">{selectedItem ? selectedItem : label}</div>
-//         {isOpen ? (
-//           <ion-icon name="chevron-up-outline"></ion-icon>
-//         ) : (
-//           <ion-icon name="chevron-down-outline"></ion-icon>
-//         )}
-//       </div>
-//       {isOpen && (
-//         <div className={styles.dropDown_items}>
-//           {items.map((item, i) => (
-//             <div
-//               className={styles.dropDown_item}
-//               key={i}
-//               onClick={() => {
-//                 setSelectedItem(item);
-//                 callback(item);
-//               }}
-//             >
-//               {item}
-//             </div>
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
+  return (
+    <div
+      tabIndex={0}
+      onBlur={(e) => {
+        setIsOpen(false);
+      }}
+    >
+      <div className={styles.dropDown} onClick={() => setIsOpen(!isOpen)}>
+        <div className="mr5">{selectedItem ? selectedItem : label}</div>
+        {isOpen ? (
+          <ion-icon name="chevron-up-outline"></ion-icon>
+        ) : (
+          <ion-icon name="chevron-down-outline"></ion-icon>
+        )}
+      </div>
+      {isOpen && (
+        <div className={styles.dropDown_items}>
+          {items.map((item, i) => (
+            <div
+              className={styles.dropDown_item}
+              key={i}
+              onClick={() => {
+                setSelectedItem(item);
+                callback(item);
+              }}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
-// export const Book = ({ book, isInside = false }) => {
-//   const { id, name, image, author } = book;
+export const Book = ({ book, isInside = false }) => {
+  const { id, name, image, author } = book;
 
-//   return (
-//     <Link href={`/book/${id}`}>
-//       <div className={styles.book}>
-//         <div className={styles.book_image}>
-//           {/* <img src={image.url} /> */}
-//           {image && <img src={`${baseUrl}${image.url}`} height="100px" />}
-//         </div>
-//         <div className={styles.book_name}>{name}</div>
-//         <div className={styles.book_author}>By {author}</div>
-//       </div>
-//     </Link>
-//   );
-// };
+  return (
+    <Link href={`/book/${id}`}>
+      <div className={styles.book}>
+        <div className={styles.book_image}>
+          {/* <img src={image.url} /> */}
+          {image && <img src={`${baseUrl}${image.url}`} height="100px" />}
+        </div>
+        <div className={styles.book_name}>{name}</div>
+        <div className={styles.book_author}>By {author}</div>
+      </div>
+    </Link>
+  );
+};
 
-// const Books = () => {
-//   const { data, loading, error } = useQuery(GET_BOOKS);
+const Books = () => {
+  const { data, loading, error } = useQuery(GET_BOOKS);
 
-//   const gql_data = data && normalize(data);
+  const gql_data = data && normalize(data);
 
-//   const { data: realmData, realmsLoading } = useQuery(GET_REALMS);
-//   const realms = realmData && normalize(realmData).realms;
+  const { data: realmData, realmsLoading } = useQuery(GET_REALMS);
+  const realms = realmData && normalize(realmData).realms;
 
-//   const [filter, setFilter] = useState(false);
-//   const [query, setQuery] = useState(false);
+  const [filter, setFilter] = useState(false);
+  const [query, setQuery] = useState(false);
 
-//   const filterBooks = (books, filter, query) => {
-//     if (query) {
-//       return books.filter((b) =>
-//         b.name.toLowerCase().includes(query.toLowerCase())
-//       );
-//     }
-//     if (!filter || filter === "All") {
-//       return books;
-//     }
+  const filterBooks = (books, filter, query) => {
+    if (query) {
+      return books.filter((b) =>
+        b.name.toLowerCase().includes(query.toLowerCase())
+      );
+    }
+    if (!filter || filter === "All") {
+      return books;
+    }
 
-//     return books.filter((b) => b.realm.name === filter);
-//   };
+    return books.filter((b) => b.realm.name === filter);
+  };
 
-//   return (
-//     <div className="background_dark">
-//       <Header />
+  return (
+    <div className="background_dark">
+      <Header />
 
-//       <div className="section">
-//         {error && <div>Error: {error}</div>}
-//         {(loading || realmsLoading) && <div>Loading...</div>}
+      <div className="section">
+        {error && <div>Error: {error}</div>}
+        {(loading || realmsLoading) && <div>Loading...</div>}
 
-//         <div className={styles.filters}>
-//           <input
-//             onChange={(e) => setQuery(e.target.value)}
-//             className={styles.searchInput}
-//             placeholder="Search..."
-//           />
+        <div className={styles.filters}>
+          <input
+            onChange={(e) => setQuery(e.target.value)}
+            className={styles.searchInput}
+            placeholder="Search..."
+          />
 
-//           {realms && (
-//             <DropDown realms={realms} filter={filter} setFilter={setFilter} />
-//           )}
-//         </div>
+          {realms && (
+            <DropDown realms={realms} filter={filter} setFilter={setFilter} />
+          )}
+        </div>
 
-//         {gql_data && (
-//           <div className={styles.grid}>
-//             {filterBooks(gql_data.books, filter, query).map((book, i) => (
-//               <Book book={book} key={i} />
-//             ))}
-//           </div>
-//         )}
-//       </div>
+        {gql_data && (
+          <div className={styles.grid}>
+            {filterBooks(gql_data.books, filter, query).map((book, i) => (
+              <Book book={book} key={i} />
+            ))}
+          </div>
+        )}
+      </div>
 
-//       <NavBar />
-//     </div>
-//   );
-// };
+      <NavBar />
+    </div>
+  );
+};
 
-// export default Books;
+export default Books;
